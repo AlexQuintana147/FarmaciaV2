050fddc579c481e3a58876333f37b68676f7e57b
AlexQuintana147 <alexquintana12622@hotmail.com>
2025-04-23 14:01:14 -0500

    nombrerutas

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\TrabajadorAuthController;
 use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('home');
@@ -37,16 +27,16 @@ Route::get('/blog', function () {
     return view('blog', compact('blogs'));
 });
 
-// Rutas de autenticación para trabajadores
+//Trabajadores
 Route::get('/login', [TrabajadorAuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [TrabajadorAuthController::class, 'login']);
 Route::post('/logout', [TrabajadorAuthController::class, 'logout'])->name('logout');
 
-// Rutas del dashboard protegidas por autenticación
+//Auth
 Route::middleware(['auth:trabajador'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     
-    // Rutas para blogs
+    //Blogs
     Route::get('/dashboard/blogs', [App\Http\Controllers\BlogController::class, 'index'])->name('dashboard.blogs');
     Route::get('/dashboard/blogs/create', [App\Http\Controllers\BlogController::class, 'create'])->name('blogs.create');
     Route::post('/dashboard/blogs', [App\Http\Controllers\BlogController::class, 'store'])->name('blogs.store');
@@ -55,11 +45,11 @@ Route::middleware(['auth:trabajador'])->group(function () {
     Route::put('/dashboard/blogs/{blog}', [App\Http\Controllers\BlogController::class, 'update'])->name('blogs.update');
     Route::delete('/dashboard/blogs/{blog}', [App\Http\Controllers\BlogController::class, 'destroy'])->name('blogs.destroy');
     
-    // Rutas para productos
+    //Productos
     Route::get('/dashboard/productos', [App\Http\Controllers\ProductoController::class, 'index'])->name('dashboard.productos');
     Route::get('/dashboard/productos/create', [App\Http\Controllers\ProductoController::class, 'create'])->name('productos.create');
     
-    // Rutas para trabajadores
+    //Trabajadores-Dash
     Route::get('/dashboard/trabajadores', [App\Http\Controllers\TrabajadorController::class, 'index'])->name('dashboard.trabajadores');
     Route::get('/dashboard/trabajadores/create', [App\Http\Controllers\TrabajadorController::class, 'create'])->name('trabajadores.create');
     Route::post('/dashboard/trabajadores', [App\Http\Controllers\TrabajadorController::class, 'store'])->name('trabajadores.store');
@@ -67,7 +57,7 @@ Route::middleware(['auth:trabajador'])->group(function () {
     Route::get('/dashboard/trabajadores/{trabajador}/edit', [App\Http\Controllers\TrabajadorController::class, 'edit'])->name('trabajadores.edit');
     Route::put('/dashboard/trabajadores/{trabajador}', [App\Http\Controllers\TrabajadorController::class, 'update'])->name('trabajadores.update');
     Route::delete('/dashboard/trabajadores/{trabajador}', [App\Http\Controllers\TrabajadorController::class, 'destroy'])->name('trabajadores.destroy');
-    // Rutas para productos
+    //Productos-Dash
     Route::post('/dashboard/productos', [App\Http\Controllers\ProductoController::class, 'store'])->name('productos.store');
     Route::get('/dashboard/productos/{producto}', [App\Http\Controllers\ProductoController::class, 'show'])->name('productos.show');
     Route::get('/dashboard/productos/{producto}/edit', [App\Http\Controllers\ProductoController::class, 'edit'])->name('productos.edit');
